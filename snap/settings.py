d0433421a76d9590fe6e1a64d0867bebf5784ed0
Kacy James <inquen@gmail.com>
2013-02-22 11:07:53 -0500

    Added local settings, updated gitignore

--- snap/settings.py
+++ snap/settings.py
@@ -1,6 +1,6 @@
 # Django settings for snap project.
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+DATABASES =  {'default':dj_database_url.config()}
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
 
 DEBUG = True
@@ -12,16 +12,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+try:
+    from local_settings import *
+except ImportError:
+    pass
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
