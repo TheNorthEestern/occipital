4b587dc7cbf7dc7975bf9b213d1ae10c0bf23eab
Kacy James <inquen@gmail.com>
2013-03-21 13:31:40 -0400

    Turns out django.views.generic.simple was deprecated, switched to TemplateView

--- trelloclone/urls.py
+++ trelloclone/urls.py
@@ -1,6 +1,6 @@
 # from trelloclone.api import BoardResource, CardResource, UserResource
 from django.conf.urls import patterns, include, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import login
 from rest_framework.urlpatterns import format_suffix_patterns
@@ -8,7 +8,7 @@ import views
 from .decorators import anonymous_required
 
 urlpatterns = patterns('',
-    url(r'^$', login_required(direct_to_template), {'template':'index/index.html'} ),
+    url(r'^$', login_required(TemplateView.as_view(template_name='index/index.html'))),
     url(r'accounts/login/$', anonymous_required(login)),
     url(r'api/v1/$', views.api_root),
     url(r'api/v1/boards/$', views.BoardList.as_view(), name='board-list'),
