97530788e115bb2bb1fb36db36fa539758e43e79
Kacy James <inquen@gmail.com>
2013-07-05 16:23:23 -0400

    removed overridden create method in view

--- trelloclone/views.py
+++ trelloclone/views.py
@@ -41,19 +41,6 @@ class BoardList(generics.ListCreateAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     #renderer_classes = (EmberJSONRenderer,)
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.DATA, files=request.FILES)
-
-        if serializer.is_valid():
-            self.pre_save(serializer.object)
-            self.object = serializer.save()
-            self.post_save(self.object, created=True)
-            headers = self.get_success_headers(serializer.data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED,
-                            headers=headers)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
     def get_queryset(self):
         user = self.request.user
         path = self.request.get_full_path()
